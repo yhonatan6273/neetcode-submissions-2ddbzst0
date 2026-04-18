2d3d77c1589fee13b739d0131d8d88ee424fdead
yhonatan berckovich <154059982+yhonatan6273@users.noreply.github.com>
2026-04-18 11:31:56 +0300

    Add: course-schedule - submission-15

---
+++ Data Structures & Algorithms/course-schedule/submission-15.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        visit=set()
+        Hash_table=defaultdict(list)
+       
+        for w,n in prerequisites:
+            Hash_table[w].append(n)
+        def dfs(i):
+            if i in visit:
+                return False
+            
+            visit.add(i)
+            for child in Hash_table[i]:
+                if not dfs(child):
+                    return False
+            visit.remove(i)
+            return True
+            
+
+        
+        for i in range(numCourses):
+            if not dfs(i):
+                return False
+        return True
+
