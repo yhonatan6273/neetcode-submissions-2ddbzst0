c1f3d0759644a7be857fee5d5884554f7820b4f0
Yehonatan Berckovich <154059982+yhonatan6273@users.noreply.github.com>
2026-07-11 18:54:48 +0300

    Add: lru-cache - submission-8

---
+++ Data Structures & Algorithms/lru-cache/submission-8.py
@@ -0,0 +1,55 @@
+class Node:
+    def __init__(self,key,val):
+        self.key=key
+        self.val=val
+        self.next=None
+        self.prev=None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity=capacity
+        self.cache={}
+        self.dummyL=Node(0,0)
+        self.dummyR=Node(0,0)
+        self.dummyL.next=self.dummyR
+        self.dummyR.prev=self.dummyL
+        
+
+    def remove(self,node:Node):
+        prv,nxt=node.prev,node.next
+        prv.next=nxt
+        nxt.prev=prv
+
+    def insert(self,node:Node):
+        prv,nxt=self.dummyR.prev,self.dummyR
+        nxt.prev=node
+        node.next=nxt
+        prv.next=node
+        node.prev=prv
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        self.remove(self.cache[key])
+        self.insert(self.cache[key])
+        return self.cache[key].val
+        
+
+        
+
+    def put(self, key: int, value: int) -> None:
+        if key not in self.cache:
+            if self.capacity==len(self.cache):
+                remove_node=self.dummyL.next
+                self.remove(remove_node)
+                del  self.cache[remove_node.key]
+            self.cache[key]=Node(key,value)
+            self.insert(self.cache[key])
+
+        else:
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            self.cache[key].val=value
+
+        
